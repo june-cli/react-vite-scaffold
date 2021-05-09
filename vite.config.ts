e0485a04d223059e4f745762b9fc2514f7afeedd
shuangmianxiaoQ <1245814811@qq.com>
2021-05-10 00:29:54 +0800

    feat: build deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,48 @@
 import path from 'path'
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: name => `antd/es/${name}/style`,
+export default ({ mode }) => {
+  // 参考：https://github.com/vitejs/vite/issues/1930#issuecomment-778595832
+  dotenv.config({ path: `.env.${mode}` })
+
+  console.log('BUILD_ENV: ', process.env.BUILD_ENV)
+
+  return defineConfig({
+    base: process.env.PUBLIC_URL,
+    plugins: [
+      reactRefresh(),
+      styleImport({
+        libs: [
+          {
+            libraryName: 'antd',
+            esModule: true,
+            resolveStyle: name => `antd/es/${name}/style`,
+          },
+        ],
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
         },
-      ],
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true
-      }
-    }
-  },
-  server: {
-    open: true,
-    proxy: {},
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-})
+    server: {
+      open: true,
+      proxy: {},
+    },
+    build: {
+      sourcemap: process.env.BUILD_ENV === 'test'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  })
+}
